9946920e3e77dfe66407281222a1caed88b9cb18
eppro <pranjal.ostwal.po@gmail.com>
2019-06-10 22:50:52 +0530

    first commit

---
+++ hash.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+const {MD5} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+ 
+// bcrypt.genSalt(10,(err,salt)=>{
+//     if(err) return next(err);
+
+//     bcrypt.hash('123456',salt,(err,hash)=>{
+//         if(err) return next(err);
+//         console.log(hash)
+//     })
+// })
+
+const secret = 'mysecretpassword';
+
+const user = {
+    id:1,
+    token: MD5('123456').toString()
+}
+
+console.log(user)
